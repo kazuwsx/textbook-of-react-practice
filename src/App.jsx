93a261b002f756f217175bfbea47f5160a22c65c
KazukiOgura <kazuki.ogura@whitecross.co.jp>
2021-12-25 14:47:35 +0900

    4-7 再レンダリングと副作用(useEffect)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
 
 export const App = () => {
+    console.log("レンダリング");
+
     const [num, setNum] = useState(0);
     const onClickButton = () => {
         setNum((prev) => prev + 1);
     }
+    useEffect(() => {
+        alert(num);
+    }, [num]);
 
     return (
         <>
